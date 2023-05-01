1a7d50c65c1394ed8a356d773bb9ce663c1530c4
Fernanda Santos de Paula <fushikai.00@gmail.com>
2023-05-01 11:38:15 -0300

    Adicionado o screenshot no script

--- TesteAgi.py
+++ TesteAgi.py
@@ -9,12 +9,21 @@ BASE_URL= "https://blogdoagi.com.br/"
 nav = webdriver.Chrome()
 nav.get(BASE_URL)
 nav.maximize_window()
+screenshot_num = 0
+
 
 wait = WebDriverWait(nav, 25)
 searchbtn = wait.until(EC.visibility_of_element_located((By.ID, 'search-open')))
 searchbtn.click()
+# Tirando um snapshot da página
+screenshot_num += 1
+nav.save_screenshot(f"screenshot{screenshot_num}.png")
 #campos abaixo não possui ID e NAME, por essa razão estou usando o xpath pois o css selector também não é boa pratica por causa do DOM.
 nav.find_element(By.XPATH, '//*[@id="masthead"]/div[1]/div[2]/form/label/input').send_keys('Sua Carreira')
+screenshot_num += 1
+nav.save_screenshot(f"screenshot{screenshot_num}.png")
 submetebtn = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="masthead"]/div[1]/div[2]/form/input')))
 submetebtn.click()
+screenshot_num += 1
+nav.save_screenshot(f"screenshot{screenshot_num}.png")
 nav.quit()
